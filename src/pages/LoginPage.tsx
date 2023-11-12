36ce3099b42d4c7e0854e176c3144b8db72d8c47
lostgirrlll <13521030@std.stei.itb.ac.id>
2023-11-12 13:11:20 +0700

    feat: edit login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -13,20 +13,20 @@ const LoginPage: React.FC = () => {
   };
 
   return (
-    <Container className="ms-auto d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
-        <Container className="d-flex flex-column justify-content-center bg-white rounded-4" style={{ height: '550px', width: '450px'}}>
+    <Container fluid className="ms-auto d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
+        <Container className="d-flex flex-column justify-content-center rounded-4 shadow" style={{ height: '550px', width: '450px', backgroundColor: '#121212' }}>
             <Container className="d-flex flex-column align-items-center">
                 <img
-                    src="../../public/logo.png"
+                    src="../../public/logo_premium.png"
                     height="120"
                     className="d-inline-block mb-4"
                     alt="logo jahitin"
                 />
-                <h1 className="mb-4 font-weight-bolder">Login</h1>
+                <h1 className="text-white mb-5 font-weight-bolder">Login</h1>
                 <Form className="d-flex flex-column align-items-center" onSubmit={handleLogin} style={{ width: '100%' }}>
-                    <Form.Group className="mb-3 px-5" controlId="formBasicUsername" style={{ width: '100%' }}>
-                    <Form.Label>Username</Form.Label>
+                    <Form.Group className="mb-4 px-5" controlId="formBasicUsername" style={{ width: '100%' }}>
                     <Form.Control
+                        className="rounded-5 px-4 py-2 border-2"
                         type="username"
                         placeholder="Username"
                         value={username}
@@ -35,8 +35,8 @@ const LoginPage: React.FC = () => {
                     </Form.Group>
 
                     <Form.Group className="mb-3 px-5" controlId="formBasicPassword" style={{ width: '100%' }}>
-                    <Form.Label>Password</Form.Label>
                     <Form.Control
+                        className="rounded-5 px-4 py-2 border-2"
                         type="password"
                         placeholder="Password"
                         value={password}
@@ -45,31 +45,46 @@ const LoginPage: React.FC = () => {
                     </Form.Group>
 
                     <Button
-                      className="mt-4 py-2 px-5 rounded-5"
-                      variant="primary"
-                      type="submit"
-                      style={{
-                        background: '#279864',
-                        color: '#fff',
-                        border: 'none',
-                        outline: 'none',
-                      }}
-                      // Efek hover
-                      onMouseOver={(e) => {
-                        e.currentTarget.style.scale = '1.1';
-                      }}
-                      onMouseOut={(e) => {
-                        e.currentTarget.style.scale = '1';
-                      }}
+                        className="mt-4 py-2 px-5 rounded-5"
+                        variant="primary"
+                        type="submit"
+                        style={{
+                            background: '#279864',
+                            color: '#fff',
+                            border: 'none',
+                            outline: 'none',
+                            fontWeight: 'bold',
+                            transition: 'transform 0.3s ease',
+                        }}
+                        onMouseOver={(e) => {
+                        e.currentTarget.style.transform = 'scale(1.05)';
+                        }}
+                        onMouseOut={(e) => {
+                        e.currentTarget.style.transform = 'scale(1)';
+                        }}
                     >
-                      Login
+                        Login
                     </Button>
 
-                    <p className="mt-3">
-                      Don't Have an Account?{' '}
-                      <Link to="/register" style={{ color: '#279864', textDecoration: 'none' }}>
-                        Register Here
-                      </Link>
+                    <p className="mt-3 text-white">
+                        Don't Have an Account?{' '}
+                        <Link
+                            to="/register"
+                            style={{
+                            color: '#279864',
+                            textDecoration: 'none',
+                            fontWeight: 'bold',
+                            transition: 'transform 0.3s ease', 
+                            display: 'inline-block',
+                            }}
+                            onMouseOver={(e) => {
+                            e.currentTarget.style.transform = 'scale(1.05)';
+                            }}
+                            onMouseOut={(e) => {
+                            e.currentTarget.style.transform = 'scale(1)'; 
+                            }}>
+                            Register Here
+                        </Link>
                     </p>
                 </Form>
             </Container>
