f74fef7ae018076ebee87ef0ad1ab82b5a226413
lostgirrlll <13521030@std.stei.itb.ac.id>
2023-11-12 13:10:59 +0700

    feat: edit app

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import { Routes, Route, useLocation } from 'react-router-dom';
 
 import NavbarComponent from './components/NavbarComponent';
 
-import HomePage from './pages/HomePage';
+import SubsManagePage from './pages/SubsManagePage';
 import ProfilPage from './pages/ProfilPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import AddImagePage from './pages/AddImagePage';
+import EditImagePage from './pages/EditImagePage';
+import GalleryManagePage from './pages/GalleryManagePage';
 
 const App: React.FC = () => {
   const location = useLocation();
@@ -18,10 +21,15 @@ const App: React.FC = () => {
     <div>
       {isNavbarVisible && <NavbarComponent />}
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/subscription" element={<SubsManagePage />} />
         <Route path="/profil" element={<ProfilPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/add" element={<AddImagePage />} />
+        {/* <Route path="/edit/:id" element={<EditImagePage />} /> */}
+        <Route path="/edit" element={<EditImagePage />} />
+
+        <Route path="/manage" element={<GalleryManagePage />} />
       </Routes> 
     </div>
   );
