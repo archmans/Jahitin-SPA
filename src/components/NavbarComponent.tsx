2568c8479452b31be914c27c09cd81616849aada
lostgirrlll <13521030@std.stei.itb.ac.id>
2023-11-12 12:59:18 +0700

    feat: modif navbar

--- src/components/NavbarComponent.tsx
+++ src/components/NavbarComponent.tsx
@@ -4,22 +4,34 @@ import { Navbar, Container, Nav } from 'react-bootstrap'
 const NavbarComponent: React.FC = () => {
   return (
     <div>
-        <Navbar expand="lg" className="bg-body-transparant">
+        <Navbar expand="lg" style={{backgroundColor: '#121212'}}>
             <Container>
                 <Navbar.Brand href="#home">
                     <img
-                        src="../../public/logo.png"
-                        height="50"
+                        src="../../public/logo_premium.png"
+                        height="80"
                         className="d-inline-block align-top"
                         alt="logo jahitin"
                     />
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle className="border border-white border-3" aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ms-auto ">
-                    <Nav.Link href="#home" style={{ color: 'rgba(39, 152, 100, 1)', textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }}>Home</Nav.Link>
-                    <Nav.Link href="#link" style={{ color: 'rgba(39, 152, 100, 1)', textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }}>Link</Nav.Link>
-                    <Nav.Link href="#link" style={{ color: 'rgba(39, 152, 100, 1)', textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }}>Logout</Nav.Link>
+                <Nav className="d-flex flex-row align-items-center ms-auto">
+                    <Container className="d-flex flex-column">
+                        <Navbar.Text className="text-white fw-bold fs-3 py-0" >hi, Tifa!</Navbar.Text>
+                        <Navbar.Text className="text-white py-0" >Admin</Navbar.Text>
+                    </Container>
+                    <Nav.Link className="text-white fw-bold border border-white border-2 rounded-2 px-4 py-2 ms-4" href="#link"
+                        style={{
+                            transition: 'background 0.3s ease',
+                          }}
+                          onMouseOver={(e) => {
+                            e.currentTarget.style.background = '#dc3545'; // Ganti dengan warna merah atau nilai warna yang diinginkan
+                          }}
+                          onMouseOut={(e) => {
+                            e.currentTarget.style.background = 'transparent';
+                          }}
+                    >Logout</Nav.Link>
                 </Nav>
                 </Navbar.Collapse>
             </Container>
