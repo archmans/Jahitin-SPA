4b74855cc5a593b0e603bcc52b1ec7ab9c96214d
Muhamad Salman Hakim Alfarisi <13521010@std.stei.itb.ac.id>
2023-11-13 11:24:06 +0700

    feat: add token on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
-
+import React, { useState, useEffect }  from 'react';
+import { Routes, Route, useLocation, Navigate} from 'react-router-dom';
 import NavbarComponent from './components/NavbarComponent';
-
 import SubsManagePage from './pages/SubsManagePage';
 import ProfilPage from './pages/ProfilPage';
 import LoginPage from './pages/LoginPage';
@@ -10,12 +8,36 @@ import RegisterPage from './pages/RegisterPage';
 import AddImagePage from './pages/AddImagePage';
 import EditImagePage from './pages/EditImagePage';
 import GalleryManagePage from './pages/GalleryManagePage';
+import verifyToken from './utils/verifyToken';
 
 const App: React.FC = () => {
   const location = useLocation();
-
-  // Tentukan apakah NavbarComponent harus ditampilkan berdasarkan rute aktif
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(true);
   const isNavbarVisible = location.pathname !== '/login' && location.pathname !== '/register';
+
+  useEffect(() => {
+    const getTokenAndVerify = async () => {
+      const token = localStorage.getItem('token');
+      console.log('Token:', token);
+
+      const isValid = await verifyToken(token);
+
+      setIsAuthenticated(isValid);
+      setLoading(false);
+    };
+
+    getTokenAndVerify();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!isAuthenticated && location.pathname !== '/login' && location.pathname !== '/register') {
+    return <Navigate to="/login" />;
+  }
+
 
   return (
     <div>
